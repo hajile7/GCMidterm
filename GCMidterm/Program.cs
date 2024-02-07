8b446e1daec043e76f1a35c22f894af35ca7aba4
elijah <elireid.28@gmail.com>
2024-02-07 15:27:04 -0500

    added half of videogame list

--- GCMidterm/Program.cs
+++ GCMidterm/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Welcome
+using GCMidterm;
+
+Console.WriteLine("Welcome to the videogame store!");
+
+//Create + Print list of games
+List<Videogame> videogames = new List<Videogame>();
+videogames.AddRange(new List<Videogame>
+{
+    new Videogame("Celeste", "Platformer", "Challenging, Pixel Art, Indie",  19.99m),
+    new Videogame("Hollow Knight", "Metroidvania", "Challenging, Action, Indie",  14.99m),
+    new Videogame("Apex Legends", "FPS", "Multiplayer, Competitive, F2P",  0.00m),
+    new Videogame("Pokemon Crystal", "RPG", "Immersive, Pixel Art, Adventure",  75.00m),
+    new Videogame("Factorio", "Factory Builder", "Immersive, Problem-Solving, 2D",  35.00m),
+    new Videogame("Super Meat Boy", "Platformer", "Challenging, Pixel Art, Indie",  14.99m),
+    new Videogame("Celeste", "Platformer", "Challenging, Pixel Art, Indie",  19.99m),
+    new Videogame("Celeste", "Platformer", "Challenging, Pixel Art, Indie",  19.99m),
+    new Videogame("Celeste", "Platformer", "Challenging, Pixel Art, Indie",  19.99m),
+    new Videogame("Celeste", "Platformer", "Challenging, Pixel Art, Indie",  19.99m),
+    new Videogame("Celeste", "Platformer", "Challenging, Pixel Art, Indie",  19.99m),
+    new Videogame("Celeste", "Platformer", "Challenging, Pixel Art, Indie",  19.99m),
+});
