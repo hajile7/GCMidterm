831460e08389c9452deb7fbe10e2dd711a170402
elijah <elireid.28@gmail.com>
2024-02-07 16:36:06 -0500

    fixing git

--- GCMidterm/Videogame.cs
+++ GCMidterm/Videogame.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.Marshalling;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,7 +10,7 @@ namespace GCMidterm
     internal class Videogame
     {
         //properties
-        public string name {  get; set; }
+        public string name { get; set; }
         public string category { get; set; }
         public string description { get; set; }
         public decimal price { get; set; }
@@ -22,8 +23,20 @@ namespace GCMidterm
             description = _description;
             price = _price;
         }
+        //Purchase Method
+        public static Videogame AddPurchase(Videogame v, int quantity)
+        {
+            v.price *= quantity;
+            return v;
 
-       
+        }
+        public override string ToString()
+        {
+            string detials = $"{name} {category} {description} {price}";
+            return detials;
+        }
+
+
 
 
     }
