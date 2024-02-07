91d7465de3b3f5a895252b3f87799392a6bb21b3
elijah <elireid.28@gmail.com>
2024-02-07 15:06:36 -0500

    added Videogame class

---
+++ GCMidterm/Videogame.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCMidterm
+{
+    internal class Videogame
+    {
+        //properties
+        public string name {  get; set; }
+        public string category { get; set; }
+        public string description { get; set; }
+        public decimal price { get; set; }
+
+        //constructor 
+        public Videogame(string _name, string _category, string _description, decimal _price)
+        {
+            name = _name;
+            category = _category;
+            description = _description;
+            price = _price;
+        }
+
+
+    }
+}
