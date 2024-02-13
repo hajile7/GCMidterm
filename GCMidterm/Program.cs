6d42ea848a73a494cf02cf822c078a7e138e34c5
Dloganreeves <loganreeves1@yahoo.com>
2024-02-13 09:18:56 -0500

    added new order loop

--- GCMidterm/Program.cs
+++ GCMidterm/Program.cs
@@ -1,15 +1,21 @@
 ﻿//Welcome
 using GCMidterm;
+using System.Data;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Text.RegularExpressions;
 using System.Xml.Serialization;
 using Validtor;
-Console.WriteLine("Welcome to the videogame store!\n");
+bool runProg = true;
+while (runProg)
+{
 
-//Create + Print list of games
-List<Videogame> videogames = new List<Videogame>();
-videogames.AddRange(new List<Videogame>
+
+    Console.WriteLine("Welcome to the videogame store!\n");
+
+    //Create + Print list of games
+    List<Videogame> videogames = new List<Videogame>();
+    videogames.AddRange(new List<Videogame>
 {
     new Videogame("Celeste", "Platformer", "Challenging, Pixel Art, Indie",  19.99m),
     new Videogame("Hollow Knight", "Metroidvania", "Challenging, Action, Indie",  14.99m),
@@ -25,151 +31,153 @@ videogames.AddRange(new List<Videogame>
     new Videogame("DOOM", "FPS", "Fast-Paced, Gory, Single-Player",  19.99m),
 });
 
-//decimal to keep a running total
-decimal runningTotal = 0;
+    //decimal to keep a running total
+    decimal runningTotal = 0;
 
-//sales tax constant 
-const decimal tax = 0.06m;
-List<Videogame> cartlist = new List<Videogame>();
-//List
-//start infinite program loop
-bool runProgram = true;
-while (runProgram)
-{
-    Console.WriteLine(string.Format("{0, -24} {1, -15} {2, -40} {3, 9}", "Title", "Genre", "Tags", "Price"));
-    Console.WriteLine(string.Format("{0, -24} {1, -15} {2, -40} {3, 10}", "===========", "==========", "=============================", "======"));
-    //Displays list
-    for (int i = 0; i < videogames.Count; i++)
+    //sales tax constant 
+    const decimal tax = 0.06m;
+    List<Videogame> cartlist = new List<Videogame>();
+    //List
+    //start infinite program loop
+    bool runProgram = true;
+    while (runProgram)
     {
-        Console.WriteLine($"{i + 1,-2}. {videogames[i].ToString()}"); //the -2 here sets the minimum width of the index
+        Console.WriteLine(string.Format("{0, -24} {1, -15} {2, -40} {3, 9}", "Title", "Genre", "Tags", "Price"));
+        Console.WriteLine(string.Format("{0, -24} {1, -15} {2, -40} {3, 10}", "===========", "==========", "=============================", "======"));
+        //Displays list
+        for (int i = 0; i < videogames.Count; i++)
+        {
+            Console.WriteLine($"{i + 1,-2}. {videogames[i].ToString()}"); //the -2 here sets the minimum width of the index
+        }
+
+        //Get user input
+        //Get the game they'd like 
+        Console.Write("\nWould you like to purchase a game? Please enter its menu number: ");
+        int choice = Validator.GetInputInt(1, 12, "Please enter a valid menu choice");
+        string choice2 = videogames[choice - 1].ToString();
+        Console.WriteLine($"Great! You've selected {videogames[choice - 1].name}.");
+
+        //make separate var to store actual videogame object
+        Videogame userChoice = videogames[choice - 1];
+        //Console.WriteLine(userChoice.ToString());
+
+        //Get quantity
+        Console.Write("How many copies would you like to purchase? ");
+        int quantityChoice = Validator.GetInputInt();
+
+        // Adds desired games and quantity to list
+        for (int i = 0; i < quantityChoice; i++)
+        {
+            cartlist.Add(userChoice);
+        }
+
+        //increment our total for each purchase
+        decimal currentPurchase = Videogame.AddPurchase(userChoice, quantityChoice);
+        runningTotal += currentPurchase;
+
+        //Pass input into purchase method
+        Console.WriteLine("Thank you! That will be $" + Videogame.AddPurchase(userChoice, quantityChoice));
+        Console.WriteLine($"Your running total is {runningTotal:C}");
+
+        //see if user would like to keep shopping
+        runProgram = Validator.GetContinue("Would you like to purchase any other games?");
+        Console.Clear();
     }
 
-    //Get user input
-    //Get the game they'd like 
-    Console.Write("\nWould you like to purchase a game? Please enter its menu number: ");
-    int choice = Validator.GetInputInt(1, 12, "Please enter a valid menu choice");
-    string choice2 = videogames[choice - 1].ToString();
-    Console.WriteLine($"Great! You've selected {videogames[choice - 1].name}.");
+    //Prints out subtotal, tax, grandtotal
+    Console.WriteLine(Videogame.GrandTotalPrint(runningTotal, tax));
 
-    //make separate var to store actual videogame object
-    Videogame userChoice = videogames[choice - 1];
-    //Console.WriteLine(userChoice.ToString());
-
-    //Get quantity
-    Console.Write("How many copies would you like to purchase? ");
-    int quantityChoice = Validator.GetInputInt();
-
-    // Adds desired games and quantity to list
-    for (int i = 0; i < quantityChoice; i++)
-    {
-        cartlist.Add(userChoice);
-    }
-
-    //increment our total for each purchase
-    decimal currentPurchase = Videogame.AddPurchase(userChoice, quantityChoice);
-    runningTotal += currentPurchase;
-
-    //Pass input into purchase method
-    Console.WriteLine("Thank you! That will be $" + Videogame.AddPurchase(userChoice, quantityChoice));
-    Console.WriteLine($"Your running total is {runningTotal:C}");
-
-    //see if user would like to keep shopping
-    runProgram = Validator.GetContinue("Would you like to purchase any other games?");
-    Console.Clear();
-}
-
-//Prints out subtotal, tax, grandtotal
-Console.WriteLine(Videogame.GrandTotalPrint(runningTotal, tax));
-
-//Valid Payment choices 
-List<string> paymentoptions = new List<string>()
+    //Valid Payment choices 
+    List<string> paymentoptions = new List<string>()
 {
 "cash",
 "credit",
 "check"
 };
 
-//create grand total variable for math
-decimal grandTotal = Videogame.GrandTotalAmount(runningTotal, tax);
+    //create grand total variable for math
+    decimal grandTotal = Videogame.GrandTotalAmount(runningTotal, tax);
 
-//Asks for payment type + validate
-Console.WriteLine("How would you like to pay? Please enter cash, check, or credit");
-string paymentchoice = Validator.GetValidString(paymentoptions);
+    //Asks for payment type + validate
+    Console.WriteLine("How would you like to pay? Please enter cash, check, or credit");
+    string paymentchoice = Validator.GetValidString(paymentoptions);
 
-//payment type logic
-//cash logic 
-if (paymentchoice == "cash")
-{
-    Console.WriteLine("How much cash are you providing?");
-    decimal cashPayment = Validator.GetInputDecimal(grandTotal, "Please try again. Invalid input or insufficient funds.");
-    decimal customerChange = Videogame.GetChange(cashPayment, grandTotal); //create change var to use in receipt later
-    Console.Clear();
-    Videogame.FinalReceipt(cartlist, runningTotal, tax);
-    Console.WriteLine($"Thank you for your purchase!\nYou paid {cashPayment:C} in cash.\nYour change is {Videogame.GetChange(cashPayment, grandTotal):C}.");
-    Console.WriteLine("====================================");
+    //payment type logic
+    //cash logic 
+    if (paymentchoice == "cash")
+    {
+        Console.WriteLine("How much cash are you providing?");
+        decimal cashPayment = Validator.GetInputDecimal(grandTotal, "Please try again. Invalid input or insufficient funds.");
+        decimal customerChange = Videogame.GetChange(cashPayment, grandTotal); //create change var to use in receipt later
+        Console.Clear();
+        Videogame.FinalReceipt(cartlist, runningTotal, tax);
+        Console.WriteLine($"Thank you for your purchase!\nYou paid {cashPayment:C} in cash.\nYour change is {Videogame.GetChange(cashPayment, grandTotal):C}.");
+        Console.WriteLine("====================================");
+    }
+
+    //check logic
+    else if (paymentchoice == "check")
+    {
+        Console.WriteLine("Please enter your check number: ");
+        string checkNum = Console.ReadLine().Trim();
+        Regex pattern = new Regex(@"^[0-9]{4,6}$");
+        Match match = pattern.Match(checkNum);
+        while (!match.Success)
+        {
+            Console.WriteLine("Invalid. Please try again");
+            checkNum = Console.ReadLine().Trim();
+            match = pattern.Match(checkNum);
+        }
+        Console.Clear();
+        Videogame.FinalReceipt(cartlist, runningTotal, tax);
+        Console.WriteLine($"Thank you for your purchase!\nPaid with check {checkNum}.");
+        Console.WriteLine("====================================");
+    }
+
+    //credit logic
+    else if (paymentchoice == "credit")
+    {
+        //card num validation
+        Console.Write("Please enter your card number: ");
+        string cardNum = Console.ReadLine().Trim();
+        Regex pattern = new Regex(@"^[0-9][0-9]{3}[-.\s]?[0-9]{4}[-.\s]?[0-9]{4}[-.\s]?[0-9]{4}$");
+        Match match = pattern.Match(cardNum);
+        while (!match.Success)
+        {
+            Console.WriteLine("Invalid card number. Please try again");
+            cardNum = Console.ReadLine().Trim();
+            match = pattern.Match(cardNum);
+        }
+
+        //card expiration date validation
+        Console.Write("Expiration date: ");
+        string expDate = Console.ReadLine().Trim();
+        Regex pattern1 = new Regex(@"^(0[1-9]|1[0-2])[-/.]?(\d{2})$");
+        Match match1 = pattern1.Match(expDate);
+        while (!match1.Success)
+        {
+            Console.WriteLine("Invalid expiration date. Please try again");
+            expDate = Console.ReadLine().Trim();
+            match1 = pattern1.Match(expDate);
+        }
+
+        //card CVV validation
+        Console.Write("CVV: ");
+        string cvv = Console.ReadLine().Trim();
+        Regex pattern2 = new Regex(@"^\d{3}$");
+        Match match2 = pattern2.Match(cvv);
+        while (!match2.Success)
+        {
+            Console.WriteLine("Invalid CVV. Please try again");
+            cvv = Console.ReadLine().Trim();
+            match2 = pattern2.Match(cvv);
+        }
+        Console.Clear();
+        Videogame.FinalReceipt(cartlist, runningTotal, tax);
+        Console.WriteLine($"Thank you for your purchase!\nPaid with card {cardNum[0]}{cardNum[1]}{cardNum[2]}{cardNum[3]}-****-****-****.");
+        Console.WriteLine("====================================");
+
+    }
+    runProg = Validator.GetContinue("Would you like to start a new order");
+    Console.Clear(); 
 }
-
-//check logic
-else if (paymentchoice == "check")
-{
-    Console.WriteLine("Please enter your check number: ");
-    string checkNum = Console.ReadLine().Trim();
-    Regex pattern = new Regex(@"^[0-9]{4,6}$");
-    Match match = pattern.Match(checkNum);
-    while (!match.Success)
-    {
-        Console.WriteLine("Invalid. Please try again");
-        checkNum = Console.ReadLine().Trim();
-        match = pattern.Match(checkNum);
-    }
-    Console.Clear();
-    Videogame.FinalReceipt(cartlist, runningTotal, tax);
-    Console.WriteLine($"Thank you for your purchase!\nPaid with check {checkNum}.");
-    Console.WriteLine("====================================");
-}
-
-//credit logic
-else if (paymentchoice == "credit")
-{
-    //card num validation
-    Console.Write("Please enter your card number: ");
-    string cardNum = Console.ReadLine().Trim();
-    Regex pattern = new Regex(@"^[0-9][0-9]{3}[-.\s]?[0-9]{4}[-.\s]?[0-9]{4}[-.\s]?[0-9]{4}$");
-    Match match = pattern.Match(cardNum);
-    while (!match.Success)
-    {
-        Console.WriteLine("Invalid card number. Please try again");
-        cardNum = Console.ReadLine().Trim();
-        match = pattern.Match(cardNum);
-    }
-
-    //card expiration date validation
-    Console.Write("Expiration date: ");
-    string expDate = Console.ReadLine().Trim();
-    Regex pattern1 = new Regex(@"^(0[1-9]|1[0-2])[-/.]?(\d{2})$");
-    Match match1 = pattern1.Match(expDate);
-    while (!match1.Success)
-    {
-        Console.WriteLine("Invalid expiration date. Please try again");
-        expDate = Console.ReadLine().Trim();
-        match1 = pattern1.Match(expDate);
-    }
-
-    //card CVV validation
-    Console.Write("CVV: ");
-    string cvv = Console.ReadLine().Trim();
-    Regex pattern2 = new Regex(@"^\d{3}$");
-    Match match2 = pattern2.Match(cvv);
-    while (!match2.Success)
-    {
-        Console.WriteLine("Invalid CVV. Please try again");
-        cvv = Console.ReadLine().Trim();
-        match2 = pattern2.Match(cvv);
-    }
-    Console.Clear();
-    Videogame.FinalReceipt(cartlist, runningTotal, tax);
-    Console.WriteLine($"Thank you for your purchase!\nPaid with card {cardNum[0]}{cardNum[1]}{cardNum[2]}{cardNum[3]}-****-****-****.");
-    Console.WriteLine("====================================");
-
-}
-
